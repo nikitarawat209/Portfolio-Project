ea798bfacaa5a85c2df1e60e85b7f0b5d2c098a5
nikita rawat <nikitarawat7072@gmail.com>
2022-12-31 15:02:21 +0530

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
-import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
   <>
-  <h1>hello world</h1>
+  <Navbar/>
   </>
   )
 }
